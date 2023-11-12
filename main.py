b1becd6cec18f646a6065bb22005cc272fb532ac
SpyDer <147894683+muhammedpeygamov123@users.noreply.github.com>
2023-11-12 13:42:53 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import subprocess
+
+def password():
+    while True:
+        password_input = input('Enter password: ')
+
+        if password_input == 'Cr4ck3r':
+            usr_ask = input('Wanna clean your machine? (yes/no): ')
+
+            if usr_ask.lower() in ['yes', 'y']:
+                try:
+                    subprocess.run(['sudo', 'apt', 'clean'])
+                    subprocess.run(['sudo', 'apt-get', 'autoremove', '-y'])
+                    subprocess.run(['sudo', 'find', '/var/log/', '-type', 'f', '-name', '*.log*', '-exec', 'rm', '-f', '{}', '+'])
+                    subprocess.run(['rm', '-rf', '~/.cache/thumbnails'])
+                    subprocess.run(['sudo', 'rm', '-rf', '/tmp/*'])
+
+                    print("System cleanup complete!")
+                except subprocess.CalledProcessError as e:
+                    print(f"Error during cleanup: {e}")
+                break  # Exit the while loop if the password is correct
+            else:
+                print("Cleanup aborted.")
+                break  # Exit the while loop if the user doesn't want to clean the system
+        else:
+            print('Please Try Again!')
+
+if __name__ == "__main__":
+    password()
